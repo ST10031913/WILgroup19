74a6f4dd80e06c8c7cd45deecc07245b8c26069e
Saneliso Lehlohla <ST10031913@vcconnect.edu.za>
2024-10-21 17:58:34 +0200

    Done

--- ShadowOfHisWings/ShadowOfHisWings/ShadowOfHisWings/Models/Home.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace ShadowOfHisWings.Models
-{
-    public class Home
-    {
-        public string Title { get; set; }
-        public string WelcomeMessage { get; set; }
-        public string AboutInfo { get; set; }
-    }
-}
---
+++ shadowofhiswings/js/script.js
@@ -0,0 +1,5 @@
+// JavaScript functionality can be added here for form validation, event handling, etc.
+document.querySelector('form').addEventListener('submit', function(event) {
+    event.preventDefault();
+    alert("Thank you for your donation!");
+});
