fec3970ea48226d07e291f07fb44e53034f2df93
liam-007 <liamabraham138@gmail.com>
2024-11-05 17:43:04 +0200

    Update

---
+++ ShadowOfHisWings/ShadowOfHisWings/Controllers/AccountController.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace ShadowOfHisWings.Controllers
+{
+    public class AccountController : Controller
+    {
+        private readonly SignInManager<IdentityUser> _signInManager;
+        private readonly UserManager<IdentityUser> _userManager;
+
+        public AccountController(SignInManager<IdentityUser> signInManager, UserManager<IdentityUser> userManager)
+        {
+            _signInManager = signInManager;
+            _userManager = userManager;
+        }
+
+        // GET: Account/Login
+        [HttpGet]
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        // POST: Account/Login
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Login(string email, string password)
+        {
+            // Find the user by email
+            var user = await _userManager.FindByEmailAsync(email);
+            if (user != null)
+            {
+                // Sign in the user
+                var result = await _signInManager.PasswordSignInAsync(user.UserName, password, false, false);
+                if (result.Succeeded)
+                {
+                    return RedirectToAction("Manage", "Events");
+                }
+            }
+            ModelState.AddModelError("", "Invalid login attempt.");
+            return View();
+        }
+
+        // POST: Account/Logout
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Logout()
+        {
+            // Sign out the user
+            await _signInManager.SignOutAsync();
+            return RedirectToAction("Index", "Home");
+        }
+    }
+}
