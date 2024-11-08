41fb8403e000d999527754b232598592d6a26b1c
Saneliso Lehlohla <107107221+ST10031913@users.noreply.github.com>
2024-11-08 14:47:25 +0200

    Done

--- ShadowOfHisWings/ShadowOfHisWings/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace ShadowOfHisWings.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
